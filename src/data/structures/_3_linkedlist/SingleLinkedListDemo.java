ae9af1a53be0891c945dbe6b62458eb68a707c05
gouwenyong0000@outlook.com <gouwenyong0000@outlook.com>
2022-05-29 23:28:48 +0800

    利用栈这个数据结构，将各个节点压入到栈中，然后利用栈的先进后出的特点，就实现了逆序打印的效果

--- src/data/structures/_3_linkedlist/SingleLinkedListDemo.java
+++ src/data/structures/_3_linkedlist/SingleLinkedListDemo.java
@@ -263,5 +263,24 @@ class SingleLinkedList {
         head.next = reverseHead.next;
     }
 
+    //方式2：
+    //可以利用栈这个数据结构，将各个节点压入到栈中，然后利用栈的先进后出的特点，就实现了逆序打印的效果
+    public static void reversePrint(HeroNode head) {
+        if (head.next == null) {
+            return;//空链表，不能打印
+        }
+        //创建要给一个栈，将各个节点压入栈
+        Stack<HeroNode> stack = new Stack<HeroNode>();
+        HeroNode cur = head.next;
+        //将链表的所有节点压入栈
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next; //cur后移，这样就可以压入下一个节点
+        }
+        //将栈中的节点进行打印,pop 出栈
+        while (stack.size() > 0) {
+            System.out.println(stack.pop()); //stack的特点是先进后出
+        }
+    }
 
 }
