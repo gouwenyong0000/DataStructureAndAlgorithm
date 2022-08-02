fe10f27bbdb30c5e1a5f6740d291e8368cdc61b3
gouwenyong0000@outlook.com <gouwenyong0000@outlook.com>
2022-08-02 21:27:33 +0800

    【图】 图深度优先遍历DFS

--- src/data/structures/_10_graph/GraphTest.java
+++ src/data/structures/_10_graph/GraphTest.java
@@ -1,7 +1,8 @@
 package data.structures._10_graph;
 
+
+
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class GraphTest {
     public static void main(String[] args) {
@@ -22,7 +23,10 @@ public class GraphTest {
         graph.insertEdge(1, 3, 1); //
         graph.insertEdge(1, 4, 1); //
 
+        graph.insertEdge(2, 4, 1); //
+
         graph.showGraph();
+        graph.dfs();
 
     }
 
@@ -36,6 +40,8 @@ class Graph {
     private int[][] edges; //存储图对应的领结矩阵
     private int numOfEdges;//表示边的数目
 
+    private boolean[] isVisited;//记录节点是否被访问
+
     /**
      * @param n 顶点数
      */
@@ -43,6 +49,67 @@ class Graph {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+    public void dfs() {
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
+    }
+
+    /**
+     * 深度优先遍历DFS
+     *
+     * @param isVisited
+     * @param i         表示第几个节点   换算到行列式中第几行  从0开始  即第一行
+     */
+    private void dfs(boolean[] isVisited, int i) {
+        //首先我们访问该结点,输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将结点设置为已经访问
+        isVisited[i] = true;
+
+        //查找结点i的第一个邻接结点w
+        int w = getFirstNeighbor(i);
+        while (w != -1) {//说明有
+
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }else
+            //如果w结点已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    /**
+     * 得到第一个邻接结点的下标 w
+     *
+     * @param index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 根据前一个邻接结点的下标来获取下一个邻接结点
+     */
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
     }
 
     /**
