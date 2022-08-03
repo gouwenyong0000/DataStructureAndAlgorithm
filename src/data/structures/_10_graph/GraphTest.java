4ea63f54b6e01fa6147db68cd430acccb73eb5f8
gouwenyong0000@outlook.com <gouwenyong0000@outlook.com>
2022-08-03 23:36:51 +0800

    【图】 图广度度优先遍历 BFS

--- src/data/structures/_10_graph/GraphTest.java
+++ src/data/structures/_10_graph/GraphTest.java
@@ -1,32 +1,60 @@
 package data.structures._10_graph;
 
 
-
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GraphTest {
     public static void main(String[] args) {
-        int n = 5;
-        String vertexs[] = {"A", "B", "C", "D", "E"};
+//        int n = 5;
+//        String vertexs[] = {"A", "B", "C", "D", "E"};
+//        //创建图对象
+//        Graph graph = new Graph(n);
+//        //循环的添加顶点
+//        for (String vertex : vertexs) {
+//            graph.insertVertex(vertex);
+//        }
+//
+//        //添加边
+//        //A-B A-C B-C B-D B-E
+//        graph.insertEdge(0, 1, 1); // A-B
+//        graph.insertEdge(0, 2, 1); //
+//        graph.insertEdge(1, 2, 1); //
+//        graph.insertEdge(1, 3, 1); //
+//        graph.insertEdge(1, 4, 1); //
+//
+//        graph.insertEdge(2, 4, 1); //
+
+        int n = 8;
+        String Vertexs[] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+
         //创建图对象
         Graph graph = new Graph(n);
         //循环的添加顶点
-        for (String vertex : vertexs) {
+        for (String vertex : Vertexs) {
             graph.insertVertex(vertex);
         }
 
         //添加边
-        //A-B A-C B-C B-D B-E
-        graph.insertEdge(0, 1, 1); // A-B
-        graph.insertEdge(0, 2, 1); //
-        graph.insertEdge(1, 2, 1); //
-        graph.insertEdge(1, 3, 1); //
-        graph.insertEdge(1, 4, 1); //
-
-        graph.insertEdge(2, 4, 1); //
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
+        graph.insertEdge(3, 7, 1);
+        graph.insertEdge(4, 7, 1);
+        graph.insertEdge(2, 5, 1);
+        graph.insertEdge(2, 6, 1);
+        graph.insertEdge(5, 6, 1);
 
         graph.showGraph();
+
+        System.out.println("深度优先遍历");
+
         graph.dfs();
+        System.out.println("\n广度优先遍历");
+
+        graph.bfs();
 
     }
 
@@ -40,7 +68,7 @@ class Graph {
     private int[][] edges; //存储图对应的领结矩阵
     private int numOfEdges;//表示边的数目
 
-    private boolean[] isVisited;//记录节点是否被访问
+//    private boolean[] isVisited;//记录节点是否被访问
 
     /**
      * @param n 顶点数
@@ -49,10 +77,59 @@ class Graph {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
-        isVisited = new boolean[n];
+
     }
 
+    //遍历所有的结点，都进行广度优先搜索
+    public void bfs() {
+        boolean[] isVisited = new boolean[vertexList.size()];//记录节点是否被访问
+
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (!isVisited[i]) {
+                bfs(isVisited, i);
+            }
+        }
+    }
+
+    //对一个结点进行广度优先遍历的方法
+    private void bfs(boolean[] isVisited, int i) {
+        int u; // 表示队列的头结点对应下标
+        int w; // 邻接结点w
+        //队列，记录结点访问的顺序
+        Queue<Integer> queue = new LinkedList();
+
+        //访问结点，输出结点信息
+        System.out.print(getValueByIndex(i) + "=>");
+        //标记为已访问
+        isVisited[i] = true;
+        //将结点加入队列
+        queue.offer(i);
+
+        while (!queue.isEmpty()) {
+            //取出队列的头结点下标
+            u = queue.poll();
+            //得到第一个邻接结点的下标 w
+            w = getFirstNeighbor(u);
+            while (w != -1) {//找到
+                //是否访问过
+                if (!isVisited[w]) {
+                    System.out.print(getValueByIndex(w) + "=>");
+                    //标记已经访问
+                    isVisited[w] = true;
+                    //入队
+                    queue.offer(w);
+                }
+                //以u为前驱点，找w后面的下一个邻结点
+                w = getNextNeighbor(u, w); //体现出我们的广度优先
+            }
+        }
+
+    }
+
+
+    //深度优先遍历DFS
     public void dfs() {
+        boolean[] isVisited = new boolean[vertexList.size()];
         for (int i = 0; i < vertexList.size(); i++) {
             if (!isVisited[i]) {
                 dfs(isVisited, i);
@@ -78,9 +155,9 @@ class Graph {
 
             if (!isVisited[w]) {
                 dfs(isVisited, w);
-            }else
-            //如果w结点已经被访问过
-            w = getNextNeighbor(i, w);
+            } else {//如果w结点已经被访问过
+                w = getNextNeighbor(i, w);
+            }
         }
     }
 
